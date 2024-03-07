a4628b5c446f49fd07555afbf46260d65e375b4a
Sandeep Guggilla <sandeepguggilla@Sandeeps-MacBook-Air.local>
2024-03-07 04:18:08 -0800

    fix: FW-75 | removed unwanted variables

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,11 +1,6 @@
-import React, { useState } from "react";
-import { useNavigate } from "react-router-dom";
 
 import Logo from "../../assets/logo/logotype.png";
 import "./Header.scss";
-import { useDispatch, useSelector } from "react-redux";
-import { logoutUser } from "../../service/authService";
-import { selectName, SET_LOGIN } from "../../redux/features/auth/authSlice";
 
 const Header = () => {
   return (
